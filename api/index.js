c819005fc2fb60754523dbc5a59d6410eb91bdc1
qwertyuiop8899 <217308952+qwertyuiop8899@users.noreply.github.com>
2026-02-16 00:39:55 +0100

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1401,7 +1401,7 @@ function applyCustomFormatter(stream, result, userConfig, serviceName = 'RD', is
             },
             addon: {
                 name: 'IlCorsaroViola',
-                version: '7.2.3',
+                version: '7.3.3',
                 presetId: preset,
                 manifestUrl: null
             },
@@ -11452,7 +11452,7 @@ export default async function handler(req, res) {
 
             const manifest = {
                 id: 'community.ilcorsaroviola.ita',
-                version: '7.2.3',
+                version: '7.3.3',
                 name: addonName,
                 description: 'Streaming da UIndex, CorsaroNero DB local, Knaben e Jackettio con o senza Real-Debrid, Torbox e Alldebrid.',
                 logo: 'https://i.imgur.com/kZK4KKS.png',
@@ -13870,7 +13870,7 @@ export default async function handler(req, res) {
             const health = {
                 status: 'OK',
                 addon: 'IlCorsaroViola',
-                version: '7.2.3',
+                version: '7.3.3',
                 uptime: Date.now(),
                 cache: {
                     entries: cache.size,
